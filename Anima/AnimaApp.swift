0d008294fc7225282a30d323cebfeb50f6ba14f4
swetabh <swetabh17@hotmail.com>
2026-03-07 13:06:33 +0530

    Initial Commit

---
+++ Anima/AnimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimaApp.swift
+//  Anima
+//
+//  Created by Swetabh Singh on 07/03/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Anima/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Anima
+//
+//  Created by Swetabh Singh on 07/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
